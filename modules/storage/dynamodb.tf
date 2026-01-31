01f4f9163175f120162ba25d002f99f4eb3122b2
Yulian302 <bohomolyulian3022003@gmail.com>
2026-01-31 19:25:02 +0100

    feat: add TTL to dynamodb "uploads" table

--- modules/storage/dynamodb.tf
+++ modules/storage/dynamodb.tf
@@ -4,6 +4,11 @@ resource "aws_dynamodb_table" "uploads" {
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "upload_id"
 
+  ttl {
+    attribute_name = "expires_at"
+    enabled        = true
+  }
+
   global_secondary_index {
     name            = "user_email-index"
     projection_type = "ALL"
@@ -35,9 +40,9 @@ resource "aws_dynamodb_table" "files" {
   hash_key     = "file_id"
 
   global_secondary_index {
-    name = "owner_email-index"
+    name            = "owner_email-index"
     projection_type = "ALL"
-    hash_key = "owner_email"
+    hash_key        = "owner_email"
   }
 
   attribute {
