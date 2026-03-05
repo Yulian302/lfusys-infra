40808dfda3ef8c4f098071c788e71b93767432d1
Yulian302 <bohomolyulian3022003@gmail.com>
2026-03-05 19:36:57 +0100

    infra: Change AWS Redis eviction policy
    - Change eviction policy from default: volatile-lru to allkeys-lru to
      evict cache based on LRU rule even for keys with no TTL.

--- environments/prod/modules/storage/redis.tf
+++ environments/prod/modules/storage/redis.tf
@@ -38,8 +38,18 @@ resource "aws_elasticache_cluster" "redis" {
   engine_version       = "6.x"
   node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis6.x"
+  parameter_group_name = aws_elasticache_parameter_group.redis.name
   port                 = 6379
   security_group_ids   = [aws_security_group.redis_sg.id]
   subnet_group_name    = aws_elasticache_subnet_group.redis.name
+}
+
+resource "aws_elasticache_parameter_group" "redis" {
+  name   = "${var.environment}-lfusys-redis-cache-params"
+  family = "redis6"
+
+  parameter {
+    name  = "maxmemory-policy"
+    value = "allkeys-lru"
+  }
 }
